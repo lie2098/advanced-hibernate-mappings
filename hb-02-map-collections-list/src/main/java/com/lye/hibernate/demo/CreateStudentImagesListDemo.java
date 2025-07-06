98e4e70bfb5fedf7624dce54a2d917ecc21c58d8
Lye <lyrollorenzo@gmail.com>
2025-07-07 00:14:38 +0800

    - hibernate mapping list
    - added hibernate congiuration auto update

---
+++ hb-02-map-collections-list/src/main/java/com/lye/hibernate/demo/CreateStudentImagesListDemo.java
@@ -0,0 +1,43 @@
+package com.lye.hibernate.demo;
+
+import com.lye.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class CreateStudentImagesListDemo {
+
+    public static void main(String[] args) {
+        String password = System.getenv("pw") != null
+                ? System.getenv("pw")
+                : System.getProperty("pw");
+
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .setProperty("hibernate.connection.password", password)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        try (sessionFactory; Session session = sessionFactory.getCurrentSession()) {
+            Student tempStudent = new Student("Lye", "Lie", "lye.lie@demo.com");
+            List<String> images = tempStudent.getImages();
+
+            images.add("image1.jpg");
+            images.add("image2.jpg");
+            images.add("image3.jpg");
+            images.add("image4.jpg");
+            images.add("image4.jpg"); //Duplicate
+
+            session.beginTransaction();
+
+
+            System.out.println("Saving the student and images...");
+            session.persist(tempStudent);
+
+            session.getTransaction().commit();
+            System.out.println("Record Saved!");
+        }
+    }
+}
---
+++ hb-02-map-collections-list/src/main/java/com/lye/hibernate/demo/entity/Student.java
@@ -0,0 +1,52 @@
+package com.lye.hibernate.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image")
+    @OrderColumn
+    @Column(name = "file_name")
+    private List<String> images = new ArrayList<>();
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}

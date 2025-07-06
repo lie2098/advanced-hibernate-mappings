0f1df4bd6e68e249636c4e15da2a2cdd498f1959
Lye <lyrollorenzo@gmail.com>
2025-07-06 23:09:52 +0800

    map collections set

---
+++ hb-01-map-collections-set/src/main/java/com/lye/hibernate/demo/entity/Student.java
@@ -0,0 +1,52 @@
+package com.lye.hibernate.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image",
+            joinColumns = @JoinColumn(name = "student_id"))
+    @Column(name = "file_name")
+    private Set<String> images = new HashSet<>();
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}

0c1e7ca83d420bbfbfe9caa9f8289c39c3771335
Lye <lyrollorenzo@gmail.com>
2025-07-07 23:01:22 +0800

    mapping sorted sets

---
+++ hb-04-map-collections-sorted-set/src/main/java/com/lye/hibernate/demo/CreateStudentImagesSortedSetDemo.java
@@ -0,0 +1,42 @@
+package com.lye.hibernate.demo;
+
+import com.lye.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Set;
+
+import static com.lye.hibernate.demo.Utils.getSession;
+
+public class CreateStudentImagesSortedSetDemo {
+
+    public static void main(String[] args) {
+        Session session = getSession();
+
+        try {
+            Student tempStudent = new Student("Lye", "Lie", "lye.lie@demo.com");
+            Set<String> images = tempStudent.getImages();
+
+            images.add("image1.jpg");
+            images.add("image2.jpg");
+            images.add("image3.jpg");
+            images.add("image4.jpg");
+            images.add("image4.jpg"); //Duplicate
+            images.add("image5.jpg");
+            images.add("image5.jpg"); //Duplicate
+
+            session.beginTransaction();
+
+
+            System.out.println("Saving the student and images...");
+            session.persist(tempStudent);
+
+            session.getTransaction().commit();
+            System.out.println("Record Saved!");
+        } finally {
+            session.close();
+            session.getSessionFactory().close();
+        }
+    }
+}
---
+++ hb-04-map-collections-sorted-set/src/main/java/com/lye/hibernate/demo/entity/Student.java
@@ -0,0 +1,52 @@
+package com.lye.hibernate.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image")
+    @org.hibernate.annotations.SQLOrder("file_name DESC")
+    @Column(name = "file_name")
+    private Set<String> images = new LinkedHashSet<>();
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
